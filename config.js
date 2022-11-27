fddbf813f6fcb6dc156d743cae51772da4e7a752
papaganesha <joaopedro.ads2021@gmail.com>
2022-11-27 16:24:03 -0300

    alterando

--- config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "https://matchmaker-api-2022.herokuapp.com/api/v1/"
-
-
-
--- screens/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-  flex: 1;
-  background: #f0f0f0;
-  align-items: center;
-`;
